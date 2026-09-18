fb78fed537a8af7adda80d183a587aa7690af689
ibrahimbadour02-debug <ibrahimbadour02@gmail.com>
2026-09-18 19:18:40 +1000

    Update content.js

--- assets/content.js
+++ assets/content.js
@@ -1,1 +1,1 @@
-window.NEXA={businessName:"NexAcademia",tagline:"Learning Simplified, Success Amplified.",phone:"0416 268 034",email:"hello@nexacademia.com.au",gaMeasurementId:"",formspreeFormId:"maeybapq"};
+window.NEXA={businessName:"NexAcademia",tagline:"Learning Simplified, Success Amplified.",phone:"0416 268 034",email:"hello@nexacademia.com.au",gaMeasurementId:"G-M2QM9CKZX7",formspreeFormId:"maeybapq"};
